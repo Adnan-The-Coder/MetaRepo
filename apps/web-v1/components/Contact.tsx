3a481d14a772dde96cd38683504ed9b04161750d
Adnan <adnanali23189@gmail.com>
2025-09-16 06:47:36 +0530

    fix: deploy bug

--- apps/web-v1/components/Contact.tsx
+++ apps/web-v1/components/Contact.tsx
@@ -9,7 +9,7 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-const Contact: React.FC = () => {
+function Contact() {
   const contactRef = useRef<HTMLDivElement>(null);
 
   // Form state
