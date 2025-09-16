f329531493904e3cf0bbc16bebd9a1bd94b1d0ab
Syed Adnan Ali <93512252+Adnan-The-Coder@users.noreply.github.com>
2025-09-16 10:39:56 +0530

    Delete apps/web-v1/instrumentation.ts

--- apps/web-v1/instrumentation.ts
+++
@@ -1,8 +0,0 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME === "nodejs") {
-    const { join } = await import("path");
-    const { cwd, exit, platform } = await import("process");
-    console.log(join("a", "b"));
-    console.log(cwd());
-  }
-}
