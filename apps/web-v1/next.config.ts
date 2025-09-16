115a60cd5e8206385b5edc2227bbec703bc9bc83
Syed Adnan Ali <93512252+Adnan-The-Coder@users.noreply.github.com>
2025-09-16 10:29:25 +0530

    Update next.config.ts

--- apps/web-v1/next.config.ts
+++ apps/web-v1/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+  instrumentationHook: false,
+}
+
 };
 
 export default nextConfig;
