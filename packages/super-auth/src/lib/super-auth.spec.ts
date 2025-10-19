6a63d3171fa8ae4ca390370b3180babbe44cb8a2
Adnan <adnanali23189@gmail.com>
2025-10-19 19:00:47 +0530

    delete cobdebase

--- packages/super-auth/src/lib/super-auth.spec.ts
+++
@@ -1,7 +0,0 @@
-import { superAuth } from './super-auth';
-
-describe('superAuth', () => {
-  it('should work', () => {
-    expect(superAuth()).toEqual('super-auth');
-  });
-});
--- packages/super-auth/src/lib/super-auth.ts
+++
@@ -1,3 +0,0 @@
-export function superAuth(): string {
-  return 'super-auth';
-}
